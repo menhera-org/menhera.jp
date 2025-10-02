8733d629f05dff4e23c2ec137c2b97b6170ab07a
metastable-void <git@yuka.h09.eu>
2025-10-02 12:38:56 +0900

    contact and footer

--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -24,9 +24,12 @@ export default function PageFooter() {
                     <li><a href="https://qiita.com/organizations/menhera">Qiita</a></li>
                     <li><a href="https://www.youtube.com/channel/UC_i1DqCa3Rfmb3LrEN4mF_w">YouTube</a></li>
                 </ul>
-                <p>© {(new Date).getFullYear()} 一般社団法人生活情報基盤研究機構。他に表記が無い限り，コンテンツは <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a> ライセンスのもとで利用可能です。(「Menhera M」ロゴなど商標を除く) Menhera®、Menhera.org、「Menhera M」ロゴは一般社団法人生活情報基盤研究機構が商標として使用しております。Menhera®は一般社団法人生活情報基盤研究機構の登録商標です (登録6945732)。</p>
+                <p>© {(new Date).getFullYear()} 一般社団法人生活情報基盤研究機構。他に表記が無い限り，コンテンツは <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a> ライセンスのもとで利用可能です。(「Menhera M」ロゴなど商標を除く) Menhera®、Menhera.org、「Menhera M」ロゴは一般社団法人生活情報基盤研究機構が商標として使用しております。Menhera®は一般社団法人生活情報基盤研究機構の登録商標です (<a href="https://www.j-platpat.inpit.go.jp/c1801/TR/JP-2025-048650/40/ja">登録第6945732号</a>)。</p>
                 <p>私たちは持続可能な開発目標(SDGs)を支援しています。</p>
                 <p>法人番号：7050005013201</p>
+                <p>非営利型一般社団法人 (非営利徹底型) (公益法人等)</p>
+                <p hidden={true}>IPアドレス管理指定事業者 (第XXXX号) (JPNIC)</p>
+                <p hidden={true}>届出電気通信事業者 (第XXXXX号)</p>
             </div>
         </div>
     </>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,13 +8,16 @@ export default function ContactUs() {
             <title>お問い合わせ | 生活情報基盤研究機構 (Menhera.org)</title>
         </Head>
         <h1>お問い合わせ</h1>
+        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSeYkYU4RA5oR5XLXMrFXobNvc0JlwA1M2Vhv8Ne03NuSFjU0A/viewform">匿名ご意見フォーム</a></p>
+        <h2>電子メール</h2>
         <p>メールでのお問い合わせを受けつけています。送信先は、「board」と「menhera.com」をアットマークでつないでください。</p>
         <h2>Discord</h2>
         <p>Discord に参加してメッセージを送信すると素早く連絡することができます。リンクは <Link href="https://mnhr.org/discord">mnhr.org/discord</Link>です。</p>
         <h2>郵便でのお問い合わせ</h2>
         <p>300-0813 茨城県土浦市富士崎1-9-24 一般社団法人生活情報基盤研究機構, Japan</p>
         <h2>電話</h2>
-        <p>029-828-4147 (代表)</p>
+        <p>Tel: 029-828-4147 (代表) (「＊」につづけて内線番号を入力することで直接内線 (Menhera 電話) にかけることができます)</p>
+        <p>FAX: 029-828-4147 (同番号)</p>
         <p>+81 29 828 4147 (海外から)</p>
     </div>
 }
